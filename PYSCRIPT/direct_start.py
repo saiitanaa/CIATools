afaa58d4a51fd2521b315653764dde82df71b5a6
Saysaa <112180175+saysaa@users.noreply.github.com>
2025-12-21 17:58:27 +0100

    Add files via upload

---
+++ PYSCRIPT/direct_start.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+os.system("title CIATools - Check Start")
+
+def FIND_root_path():
+
+    root_path = os.path.abspath(os.path.dirname(__file__))
+
+    while root_path:
+        marker = os.path.join(root_path, "root_path")
+        if os.path.isfile(marker):
+            return root_path
+
+        parent = os.path.dirname(root_path)
+        if parent == root_path:
+            break
+        root_path = parent
+
+    return None 
+
+root_path = FIND_root_path()
+START_CIATools = os.path.join(root_path, "CIATools_HUD", "CIATools_HUD", "bin", "Release", "net8.0-windows")
+os.chdir(START_CIATools)
+os.system("start CIATools_HUD.exe")
