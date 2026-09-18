cb035580154259d0facf6f9ce35fcfcaa128ed4a
Saiitanaa <lgrs.lhr@proton.me>
2026-09-18 15:09:14 +0200

    Patch #1 Crossplatform Win32

--- makerom/src/lib.h
+++ makerom/src/lib.h
@@ -4,36 +4,35 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <strings.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <ctype.h>
 #include <time.h>
-#include <unistd.h>
 #include <math.h>
-#include <dirent.h>
 #include <wchar.h>
 #include <inttypes.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
-	
 #ifdef _WIN32
-	#include <io.h>
-	#include <direct.h>
-	#include <windows.h>
-#endif
 
+    #include <io.h>
+    #include <direct.h>
+    #include <windows.h>
+
+#else
+
+    #include <strings.h>
+    #include <unistd.h>
+    #include <dirent.h>
+    #include <sys/stat.h>
+    #include <sys/types.h>
+
+#endif
 
 #include "types.h"
 #include "utils.h"
 #include "ctr_utils.h"
 #include "crypto.h"
-
 #include "keyset.h"
 #include "user_settings.h"
 #include "yaml_parser.h"
 #include "rsf_settings.h"
-
-
-
